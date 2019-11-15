82f23ab3644a542f691a876c38eb503c0714610e
kendrang <kendrang@users.noreply.github.com>
2019-11-14 20:52:14 -0500

    added contains

--- src/main/java/com/zipcodewilmington/singlylinkedlist/SinglyLinkedList.java
+++ src/main/java/com/zipcodewilmington/singlylinkedlist/SinglyLinkedList.java
@@ -4,4 +4,54 @@ package com.zipcodewilmington.singlylinkedlist;
  * Created by leon on 1/10/18.
  */
 public class SinglyLinkedList {
+
+    Node head;
+
+    private class Node {
+        int data;
+        Node next;
+
+        Node(int d) {
+            data = d;
+        }
+    }
+
+    public void add(int data) {
+        if (head == null) {
+            head = new Node(data);
+        } else {
+            Node n = head;
+            while (n.next != null) {
+                n = n.next;
+
+            }
+            n.next = new Node(data);
+        }
+    }
+
+    public void remove(int data) {
+        Node current = head;
+        if (head.data == data) {
+            head = head.next;
+        }
+        while (current.next != null) {
+            if (current.next.data == data) {
+                current.next = current.next.next;
+                return;
+            }
+            current = current.next;
+        }
+    }
+
+    public boolean contains(int data) {
+        Node current = head;
+        while (current.next != null) {
+            current = current.next;
+            if (current.next.data == data) {
+                return true;
+            }
+        }return false;
+    }
+
+
 }
